955037755eb1af07e03e1323212d985610c61ea8
BananaBoyCoding <wyx6764@163.com>
2019-08-05 18:02:10 +0800

    第一次提交

---
+++ app/src/main/java/com/wyx/demo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.wyx.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * @author   : lls
+ * time      : 2019/8/5
+ * describe  : 开始造轮子
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
