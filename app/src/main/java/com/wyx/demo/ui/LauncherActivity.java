5d6a41875ba5bb898ddd01c70bc1c48efad13801
BananaBoyCoding <wyx6764@163.com>
2019-08-08 15:24:50 +0800

    启动页动画

--- app/src/main/java/com/wyx/demo/ui/LauncherActivity.java
+++ app/src/main/java/com/wyx/demo/ui/LauncherActivity.java
@@ -8,60 +8,71 @@ import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.DecelerateInterpolator;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.wyx.demo.R;
+
+import butterknife.BindView;
 
 /**
  * @author : lls
  * time      : 2019/8/5
  * describe  :
  */
-public class LauncherActivity extends AppCompatActivity {
+public class LauncherActivity extends AppCompatActivity implements Animation.AnimationListener {
+    private ImageView mImageView;
+    private ImageView mIconView;
+    private TextView mTextView;
+
+    private static final int ANIM_TIME = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
-        loadAnimation();
+        initStartAnim();
     }
 
     private void initView() {
-        findViewById(R.id.aaa).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-               // startActivity(new Intent(LauncherActivity.this, HomeActivity.class));
-            }
-        });
+        mImageView = findViewById(R.id.iv_launcher_bg);
+        mIconView = findViewById(R.id.iv_launcher_icon);
+        mTextView = findViewById(R.id.iv_launcher_name);
     }
 
-    private void loadAnimation() {
-        AlphaAnimation alphaAnimation = new AlphaAnimation(0f, 1f);
-        alphaAnimation.setDuration(3000);
-        alphaAnimation.setFillAfter(true);
-        alphaAnimation.setInterpolator(new DecelerateInterpolator());
-        alphaAnimation.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
-            }
+    private void initStartAnim() {
+        // 渐变展示启动屏
+        AlphaAnimation aa = new AlphaAnimation(0.4f, 1.0f);
+        aa.setDuration(ANIM_TIME * 3);
+        aa.setAnimationListener(this);
+        mImageView.startAnimation(aa);
 
-            @Override
-            public void onAnimationEnd(Animation animation) {
+        ScaleAnimation sa = new ScaleAnimation(0, 1, 0, 1, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        sa.setDuration(ANIM_TIME);
+        mIconView.startAnimation(sa);
 
-                    Intent intent = new Intent(LauncherActivity.this, HomeActivity.class);
-                    startActivity(intent);
-                    overridePendingTransition(0, 0);
-                    finish();
+        RotateAnimation ra = new RotateAnimation(180, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        ra.setDuration(ANIM_TIME);
+        mTextView.startAnimation(ra);
+    }
 
-            }
+    @Override
+    public void onAnimationStart(Animation animation) {
 
-            @Override
-            public void onAnimationRepeat(Animation animation) {
+    }
 
-            }
-        });
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        Intent intent = new Intent(LauncherActivity.this, HomeActivity.class);
+        startActivity(intent);
+        finish();
+    }
 
-
+    @Override
+    public void onAnimationRepeat(Animation animation) {
 
     }
 }
